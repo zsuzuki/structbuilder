e5e4e3666df26a37be0ccc8fb7cd1fe0d2f92df8
鈴木 良則 <suzukiy@polyphony.co.jp>
2018-01-22 12:32:47 +0900

    build struct/

--- main.go
+++ main.go
@@ -4,19 +4,53 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/pelletier/go-toml"
 )
 
+const (
+	intSize     = 4
+	pointerSize = 8
+)
+
+var (
+	sizeMap = map[string]int64{
+		"int":                intSize,
+		"unsigned int":       intSize,
+		"size_t":             pointerSize,
+		"char":               1,
+		"unsigned char":      1,
+		"short":              2,
+		"unsigned short":     2,
+		"long":               intSize,
+		"unsigned long":      intSize,
+		"long long":          8,
+		"unsigned long long": 8,
+		"float":              4,
+		"double":             8,
+		"int8_t":             1,
+		"uint8_t":            1,
+		"int16_t":            2,
+		"uint16_t":           2,
+		"int32_t":            4,
+		"uint32_t":           4,
+		"int64_t":            8,
+		"uint64_t":           8,
+	}
+)
+
 // MemberInfo struct member's information
 type MemberInfo struct {
-	Name          string
-	Type          string
-	Array         int64
-	Default       interface{}
-	DefaultString string
-	Bits          int64
-	Comment       string
+	Name    string
+	Type    string
+	Array   int64
+	Default string
+	Bits    int64
+	Comment string
+	Offset  int64
+	Size    int64
 }
 
 // StructInfo single struct information
@@ -30,6 +64,7 @@ type StructInfo struct {
 // (namespace,include...)
 type GlobalInfo struct {
 	NameSpace    string
+	HasNameSpace bool
 	Include      []string
 	LocalInclude []string
 	Struct       StructInfo
@@ -60,10 +95,47 @@ func getInt(c *toml.Tree, n string, d int64) int64 {
 	return r.(int64)
 
 }
+func getFloat(c *toml.Tree, n string, d float64) float64 {
+	r := c.Get(n)
+	if r == nil {
+		return d
+	}
+	return r.(float64)
+}
+
+//
+func checkFloatType(typeString string) bool {
+	switch typeString {
+	case "float", "double":
+		return true
+	}
+	return false
+}
+
+//
+func checkStringType(typeString string, arraySize int64) bool {
+	switch typeString {
+	case "std::string":
+		return true
+	case "char", "unsigned char":
+		if arraySize > 1 {
+			return true
+		}
+	}
+	if strings.Index(typeString, "char*") >= 0 {
+		return true
+	}
+	if strings.Index(typeString, "char *") >= 0 {
+		return true
+	}
+	return false
+}
 
 // parse member
 func memberParse(tomlConfig *toml.Tree) ([]MemberInfo, error) {
 	var membersInfo []MemberInfo
+	var offset int64
+	offset = 0
 	membersConfig := tomlConfig.Get("member")
 	if membersConfig != nil {
 		membersConfigList := membersConfig.([]*toml.Tree)
@@ -80,10 +152,34 @@ func memberParse(tomlConfig *toml.Tree) ([]MemberInfo, error) {
 				Name:    mName.(string),
 				Type:    mType.(string),
 				Bits:    getInt(member, "bits", 0),
+				Array:   getInt(member, "array", 1),
 				Comment: getString(member, "comment"),
 			}
+			baseSize, foundType := sizeMap[m.Type]
+			if foundType == false {
+				if strings.Index(m.Type, "*") >= 0 {
+					baseSize = pointerSize
+				} else {
+					baseSize = intSize
+				}
+			}
+			alignSize := baseSize - (offset % baseSize)
+			if baseSize != alignSize {
+				m.Offset = offset + alignSize
+			} else {
+				m.Offset = offset
+			}
+			m.Size = baseSize * m.Array
+			offset = m.Offset + m.Size
+			if checkStringType(m.Type, m.Array) {
+				m.Default = getString(member, "default")
+			} else if checkFloatType(m.Type) {
+				m.Default = strconv.FormatFloat(getFloat(member, "default", 0), 'e', -1, 64)
+			} else {
+				m.Default = strconv.Itoa(int(getInt(member, "default", 0)))
+			}
 			membersInfo = append(membersInfo, m)
-			fmt.Println("    ", mType, mName, ";")
+			fmt.Println("    ", mType, mName, "; //", m.Offset, ":", m.Size, "=", m.Default)
 		}
 	}
 	return membersInfo, nil
@@ -95,6 +191,12 @@ func parseToml(tomlConfig *toml.Tree) (GlobalInfo, error) {
 	wInfo.Include = []string{}
 	wInfo.LocalInclude = []string{}
 	wInfo.NameSpace = getString(tomlConfig, "namespace")
+	if wInfo.NameSpace == "" {
+		wInfo.HasNameSpace = false
+	} else {
+		wInfo.HasNameSpace = true
+	}
+
 	il := tomlConfig.Get("include")
 	if il != nil {
 		includeList := il.([]interface{})
@@ -158,6 +260,8 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println("namespace:", wInfo.NameSpace)
-	fmt.Printf("struct %s {}\n", wInfo.Struct.Name)
+	if wInfo.HasNameSpace {
+		fmt.Println("namespace:", wInfo.NameSpace)
+	}
+	fmt.Printf("struct %s\n", wInfo.Struct.Name)
 }
