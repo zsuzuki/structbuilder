e7dc4cd69b75010cd9efa445d5431aae86c87b29
Yoshinori Suzuki <wave.suzuki.z@gmail.com>
2018-10-19 04:20:49 +0900

    cpp format by LLVM

--- cpp/sample.cpp
+++ cpp/sample.cpp
@@ -1,59 +1,52 @@
 #include "serializer.hpp"
 
 #include <cstdint>
+#include <iostream>
 #include <string>
 #include <vector>
-#include <iostream>
 
-int main(int argc, char **argv)
-{
-    try
-    {
-        std::cout << "Start" << std::endl;
-        auto ser = Serializer{};
-        char buffer[128];
-        ser.initialize(buffer, sizeof(buffer));
+int main(int argc, char **argv) {
+  try {
+    std::cout << "Start" << std::endl;
+    auto ser = Serializer{};
+    char buffer[128];
+    ser.initialize(buffer, sizeof(buffer));
 
-        // number test
-        static constexpr int nb_num = 32;
-        for (int i = 0; i < nb_num; i++)
-        {
-            ser.put<int>(i * 2);
-        }
-        std::cout << "put number done: " << ser.getWriteSize() << " bytes" << std::endl;
-        for (int i = 0; i < nb_num; i++)
-        {
-            std::cout << " " << ser.get<int>();
-        }
-        std::cout << "\nget number done: " << ser.getReadSize() << " bytes" << std::endl;
-
-        // string test
-        ser.reset();
-        std::vector<const char *> str = {
-            "Hello, World",
-            "Serializer is serializer",
-            "Buffer type <char>",
-            "serialize test program",
-            "message of contents"};
-        using msg_size_t = uint8_t;
-        ser.put<msg_size_t>(str.size());
-        for (auto s : str)
-        {
-            auto sz = strlen(s) + 1;
-            ser.putBuffer<char *, msg_size_t>(const_cast<char *>(s), sz);
-        }
-        std::cout << "put string done: " << ser.getWriteSize() << " bytes" << std::endl;
-        auto nb_msg = ser.get<msg_size_t>();
-        for (int i = 0; i < nb_msg; i++)
-        {
-            auto r = ser.getBuffer<const char *, msg_size_t>();
-            std::cout << (int)r.second << ": " << r.first << std::endl;
-        }
-        std::cout << "get string done: " << ser.getReadSize() << " bytes" << std::endl;
+    // number test
+    static constexpr int nb_num = 32;
+    for (int i = 0; i < nb_num; i++) {
+      ser.put<int>(i * 2);
     }
-    catch (std::exception &e)
-    {
-        std::cerr << "\n*** " << e.what() << " ***" << std::endl;
+    std::cout << "put number done: " << ser.getWriteSize() << " bytes"
+              << std::endl;
+    for (int i = 0; i < nb_num; i++) {
+      std::cout << " " << ser.get<int>();
     }
-    return 0;
+    std::cout << "\nget number done: " << ser.getReadSize() << " bytes"
+              << std::endl;
+
+    // string test
+    ser.reset();
+    std::vector<const char *> str = {
+        "Hello, World", "Serializer is serializer", "Buffer type <char>",
+        "serialize test program", "message of contents"};
+    using msg_size_t = uint8_t;
+    ser.put<msg_size_t>(str.size());
+    for (auto s : str) {
+      auto sz = strlen(s) + 1;
+      ser.putBuffer<char *, msg_size_t>(const_cast<char *>(s), sz);
+    }
+    std::cout << "put string done: " << ser.getWriteSize() << " bytes"
+              << std::endl;
+    auto nb_msg = ser.get<msg_size_t>();
+    for (int i = 0; i < nb_msg; i++) {
+      auto r = ser.getBuffer<const char *, msg_size_t>();
+      std::cout << (int)r.second << ": " << r.first << std::endl;
+    }
+    std::cout << "get string done: " << ser.getReadSize() << " bytes"
+              << std::endl;
+  } catch (std::exception &e) {
+    std::cerr << "\n*** " << e.what() << " ***" << std::endl;
+  }
+  return 0;
 }
--- cpp/serializer.hpp
+++ cpp/serializer.hpp
@@ -4,140 +4,125 @@
 #pragma once
 
 #include <cstdint>
+#include <cstring>
 #include <exception>
 #include <string>
-#include <cstring>
 
 //
-class Serializer
-{
-  private:
-    void *buffer = nullptr;
-    size_t buffer_size = 0;
-    size_t w_pointer = 0;
-    size_t r_pointer = 0;
+class Serializer {
+private:
+  void *buffer = nullptr;
+  size_t buffer_size = 0;
+  size_t w_pointer = 0;
+  size_t r_pointer = 0;
 
-    //
-    size_t get_read_max() const
-    {
-        return (w_pointer == 0 || buffer_size < w_pointer) ? buffer_size : w_pointer;
-    }
+  //
+  size_t get_read_max() const {
+    return (w_pointer == 0 || buffer_size < w_pointer) ? buffer_size
+                                                       : w_pointer;
+  }
 
-    //
-    class Exception : public std::exception
-    {
-        char error_message[256];
-
-      public:
-        Exception(bool wr, const char *msg, size_t p, size_t ds, size_t s)
-        {
-            snprintf(error_message, sizeof(error_message), "%s%s: pointer=%zu, data size=%zu buffer size=%zu",
-                     wr ? "[write]" : "[read]",
-                     msg,
-                     p,
-                     ds,
-                     s);
-        }
-        ~Exception() override = default;
-        const char *what() const noexcept override { return error_message; }
-    };
+  //
+  class Exception : public std::exception {
+    char error_message[256];
 
   public:
-    Serializer() = default;
-    ~Serializer() = default;
+    Exception(bool wr, const char *msg, size_t p, size_t ds, size_t s) {
+      snprintf(error_message, sizeof(error_message),
+               "%s%s: pointer=%zu, data size=%zu buffer size=%zu",
+               wr ? "[write]" : "[read]", msg, p, ds, s);
+    }
+    ~Exception() override = default;
+    const char *what() const noexcept override { return error_message; }
+  };
 
-    void initialize(void *b, size_t bs)
-    {
-        buffer = b;
-        buffer_size = bs;
-        reset();
-    }
+public:
+  Serializer() = default;
+  ~Serializer() = default;
 
-    void reset()
-    {
-        w_pointer = 0;
-        r_pointer = 0;
-    }
-    size_t getWriteSize() const { return w_pointer; }
-    size_t getReadSize() const { return r_pointer; }
+  void initialize(void *b, size_t bs) {
+    buffer = b;
+    buffer_size = bs;
+    reset();
+  }
 
-    // number
-    template <class T = uint32_t>
-    void put(const T &v)
-    {
-        auto next_ptr = w_pointer + sizeof(T);
-        if (next_ptr > buffer_size)
-        {
-            // buffer over
-            throw Exception(true, "[number] buffer over!", w_pointer, sizeof(T), buffer_size);
-        }
-        auto base_ptr = reinterpret_cast<uint8_t *>(buffer) + w_pointer;
-        auto ptr = reinterpret_cast<T *>(base_ptr);
-        std::memcpy(ptr, &v, sizeof(T));
-        w_pointer = next_ptr;
+  void reset() {
+    w_pointer = 0;
+    r_pointer = 0;
+  }
+  size_t getWriteSize() const { return w_pointer; }
+  size_t getReadSize() const { return r_pointer; }
+
+  // number
+  template <class T = uint32_t> void put(const T &v) {
+    auto next_ptr = w_pointer + sizeof(T);
+    if (next_ptr > buffer_size) {
+      // buffer over
+      throw Exception(true, "[number] buffer over!", w_pointer, sizeof(T),
+                      buffer_size);
     }
-    //
-    template <class T = uint32_t>
-    T get()
-    {
-        auto next_ptr = r_pointer + sizeof(T);
-        auto max_pointer = get_read_max();
-        if (next_ptr > max_pointer)
-        {
-            // buffer over
-            throw Exception(false, "[number] buffer over!", r_pointer, sizeof(T), max_pointer);
-        }
-        auto base_ptr = reinterpret_cast<uint8_t *>(buffer) + r_pointer;
-        auto ptr = reinterpret_cast<T *>(base_ptr);
-        T result;
-        std::memcpy(&result, ptr, sizeof(T));
-        r_pointer = next_ptr;
-        return result;
+    auto base_ptr = reinterpret_cast<uint8_t *>(buffer) + w_pointer;
+    auto ptr = reinterpret_cast<T *>(base_ptr);
+    std::memcpy(ptr, &v, sizeof(T));
+    w_pointer = next_ptr;
+  }
+  //
+  template <class T = uint32_t> T get() {
+    auto next_ptr = r_pointer + sizeof(T);
+    auto max_pointer = get_read_max();
+    if (next_ptr > max_pointer) {
+      // buffer over
+      throw Exception(false, "[number] buffer over!", r_pointer, sizeof(T),
+                      max_pointer);
     }
-    // buffer
-    template <class T = char *, typename TS = size_t>
-    void putBuffer(T source_buffer, TS write_size)
-    {
-        auto next_ptr = w_pointer + write_size + sizeof(TS);
-        if (next_ptr > buffer_size)
-        {
-            // buffer over
-            throw Exception(true, "[buffer] buffer over!", w_pointer, write_size + sizeof(TS), buffer_size);
-        }
-        auto base_ptr = reinterpret_cast<uint8_t *>(buffer) + w_pointer;
-        auto size_ptr = reinterpret_cast<TS *>(base_ptr);
-        auto buff_ptr = reinterpret_cast<T>(size_ptr + 1);
-        std::memcpy(size_ptr, &write_size, sizeof(TS));
-        std::memcpy(buff_ptr, source_buffer, write_size);
-        w_pointer = next_ptr;
+    auto base_ptr = reinterpret_cast<uint8_t *>(buffer) + r_pointer;
+    auto ptr = reinterpret_cast<T *>(base_ptr);
+    T result;
+    std::memcpy(&result, ptr, sizeof(T));
+    r_pointer = next_ptr;
+    return result;
+  }
+  // buffer
+  template <class T = char *, typename TS = size_t>
+  void putBuffer(T source_buffer, TS write_size) {
+    auto next_ptr = w_pointer + write_size + sizeof(TS);
+    if (next_ptr > buffer_size) {
+      // buffer over
+      throw Exception(true, "[buffer] buffer over!", w_pointer,
+                      write_size + sizeof(TS), buffer_size);
     }
-    //
-    template <class T = char *, typename TS = size_t>
-    std::pair<T, TS> getBuffer()
-    {
-        auto next_ptr = r_pointer + sizeof(TS);
-        auto max_pointer = get_read_max();
-        if (next_ptr > max_pointer)
-        {
-            // buffer over
-            throw Exception(false, "[buffer] buffer over!", r_pointer, sizeof(TS), max_pointer);
-        }
-        auto base_ptr = reinterpret_cast<uint8_t *>(buffer) + r_pointer;
-        auto size_ptr = reinterpret_cast<TS *>(base_ptr);
-        TS read_size;
-        std::memcpy(&read_size, size_ptr, sizeof(TS));
-        if (read_size > 0)
-        {
-            next_ptr += read_size;
-            if (next_ptr > max_pointer)
-            {
-                // buffer over
-                throw Exception(false, "[buffer] buffer over!", r_pointer, read_size, max_pointer);
-            }
-            auto buff_ptr = reinterpret_cast<T>(size_ptr + 1);
-            r_pointer = next_ptr;
-            return std::make_pair(buff_ptr, read_size);
-        }
-        return std::make_pair(nullptr, 0);
+    auto base_ptr = reinterpret_cast<uint8_t *>(buffer) + w_pointer;
+    auto size_ptr = reinterpret_cast<TS *>(base_ptr);
+    auto buff_ptr = reinterpret_cast<T>(size_ptr + 1);
+    std::memcpy(size_ptr, &write_size, sizeof(TS));
+    std::memcpy(buff_ptr, source_buffer, write_size);
+    w_pointer = next_ptr;
+  }
+  //
+  template <class T = char *, typename TS = size_t>
+  std::pair<T, TS> getBuffer() {
+    auto next_ptr = r_pointer + sizeof(TS);
+    auto max_pointer = get_read_max();
+    if (next_ptr > max_pointer) {
+      // buffer over
+      throw Exception(false, "[buffer] buffer over!", r_pointer, sizeof(TS),
+                      max_pointer);
     }
+    auto base_ptr = reinterpret_cast<uint8_t *>(buffer) + r_pointer;
+    auto size_ptr = reinterpret_cast<TS *>(base_ptr);
+    TS read_size;
+    std::memcpy(&read_size, size_ptr, sizeof(TS));
+    if (read_size > 0) {
+      next_ptr += read_size;
+      if (next_ptr > max_pointer) {
+        // buffer over
+        throw Exception(false, "[buffer] buffer over!", r_pointer, read_size,
+                        max_pointer);
+      }
+      auto buff_ptr = reinterpret_cast<T>(size_ptr + 1);
+      r_pointer = next_ptr;
+      return std::make_pair(buff_ptr, read_size);
+    }
+    return std::make_pair(nullptr, 0);
+  }
 };
