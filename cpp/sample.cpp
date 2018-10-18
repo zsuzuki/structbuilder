ed10aa9e0a0dc8b61a48f877d56682643f0aa5f3
Yoshinori Suzuki <wave.suzuki.z@gmail.com>
2018-10-19 04:57:33 +0900

    struct test.

--- cpp/sample.cpp
+++ cpp/sample.cpp
@@ -12,7 +12,9 @@ int main(int argc, char **argv) {
     char buffer[128];
     ser.initialize(buffer, sizeof(buffer));
 
+    //
     // number test
+    //
     static constexpr int nb_num = 32;
     for (int i = 0; i < nb_num; i++) {
       ser.put<int>(i * 2);
@@ -25,25 +27,54 @@ int main(int argc, char **argv) {
     std::cout << "\nget number done: " << ser.getReadSize() << " bytes"
               << std::endl;
 
+    //
     // string test
+    //
     ser.reset();
     std::vector<const char *> str = {
         "Hello, World", "Serializer is serializer", "Buffer type <char>",
         "serialize test program", "message of contents"};
     using msg_size_t = uint8_t;
-    ser.put<msg_size_t>(str.size());
     for (auto s : str) {
       auto sz = strlen(s) + 1;
       ser.putBuffer<char *, msg_size_t>(const_cast<char *>(s), sz);
     }
     std::cout << "put string done: " << ser.getWriteSize() << " bytes"
               << std::endl;
-    auto nb_msg = ser.get<msg_size_t>();
-    for (int i = 0; i < nb_msg; i++) {
+    for (int i = 0; i < str.size(); i++) {
       auto r = ser.getBuffer<const char *, msg_size_t>();
       std::cout << (int)r.second << ": " << r.first << std::endl;
     }
     std::cout << "get string done: " << ser.getReadSize() << " bytes"
+              << std::endl;
+
+    //
+    // bit field
+    // NOTE: ** not aligned for test **
+    //
+    struct Field {
+      unsigned enabled : 1;
+      unsigned index : 5;
+      unsigned message : 10;
+      unsigned : 14;
+    };
+    ser.reset();
+    static constexpr msg_size_t nb_f = 5;
+    for (int i = 0; i < nb_f; i++) {
+      Field f{};
+      f.enabled = i % 3 == 0;
+      f.index = i;
+      f.message = 10000 - i * 144;
+      ser.put<Field>(f);
+    }
+    std::cout << "put bit-field struct done: " << ser.getWriteSize() << " bytes"
+              << std::endl;
+    for (int i = 0; i < nb_f; i++) {
+      auto f = ser.get<Field>();
+      std::cout << "F: enabled=" << f.enabled << " index=" << f.index
+                << " message=" << f.message << std::endl;
+    }
+    std::cout << "get bit-field struct done: " << ser.getReadSize() << " bytes"
               << std::endl;
   } catch (std::exception &e) {
     std::cerr << "\n*** " << e.what() << " ***" << std::endl;
