60283371f0895a56a52240947ff063cded5690a4
Yoshinori Suzuki <wave.suzuki.z@gmail.com>
2018-10-24 07:31:00 +0900

    serializer refactoring.

--- cpp/serializer.hpp
+++ cpp/serializer.hpp
@@ -22,6 +22,14 @@ private:
     return (w_pointer == 0 || buffer_size < w_pointer) ? buffer_size
                                                        : w_pointer;
   }
+  template <class T> T *get_write_pointer() {
+    auto base_ptr = reinterpret_cast<uint8_t *>(buffer) + w_pointer;
+    return reinterpret_cast<T *>(base_ptr);
+  }
+  template <class T> T *get_read_pointer() {
+    auto base_ptr = reinterpret_cast<uint8_t *>(buffer) + r_pointer;
+    return reinterpret_cast<T *>(base_ptr);
+  }
 
   //
   class Exception : public std::exception {
@@ -36,6 +44,16 @@ private:
     ~Exception() override = default;
     const char *what() const noexcept override { return error_message; }
   };
+  size_t get_next_pointer(size_t data_size, bool is_write,
+                          const char *err_msg) const {
+    auto max_ptr = is_write ? buffer_size : get_read_max();
+    auto base_ptr = is_write ? w_pointer : r_pointer;
+    auto next_ptr = base_ptr + data_size;
+    if (next_ptr > max_ptr) {
+      throw Exception(is_write, err_msg, base_ptr, data_size, max_ptr);
+    }
+    return next_ptr;
+  }
 
 public:
   Serializer() = default;
@@ -47,6 +65,7 @@ public:
     reset();
   }
 
+  //
   void reset() {
     w_pointer = 0;
     r_pointer = 0;
@@ -56,89 +75,62 @@ public:
 
   // number
   template <class T = uint32_t> void put(const T &v) {
-    auto next_ptr = w_pointer + sizeof(T);
-    if (next_ptr > buffer_size) {
-      // buffer over
-      throw Exception(true, "[number] buffer over!", w_pointer, sizeof(T),
-                      buffer_size);
-    }
-    auto base_ptr = reinterpret_cast<uint8_t *>(buffer) + w_pointer;
-    auto ptr = reinterpret_cast<T *>(base_ptr);
+    auto next_ptr = get_next_pointer(sizeof(T), true, "[number] buffer over!");
+    auto ptr = get_write_pointer<T>();
     std::memcpy(ptr, &v, sizeof(T));
     w_pointer = next_ptr;
   }
   //
   template <class T = uint32_t> T get() {
-    auto next_ptr = r_pointer + sizeof(T);
-    auto max_pointer = get_read_max();
-    if (next_ptr > max_pointer) {
-      // buffer over
-      throw Exception(false, "[number] buffer over!", r_pointer, sizeof(T),
-                      max_pointer);
-    }
-    auto base_ptr = reinterpret_cast<uint8_t *>(buffer) + r_pointer;
-    auto ptr = reinterpret_cast<T *>(base_ptr);
+    auto next_ptr = get_next_pointer(sizeof(T), false, "[number] buffer over!");
+    auto ptr = get_read_pointer<T>();
     T result;
     std::memcpy(&result, ptr, sizeof(T));
     r_pointer = next_ptr;
     return result;
   }
+
   // buffer
   template <class T = char, typename TS = size_t>
   void putBuffer(const T *source_buffer, TS write_size) {
     auto total_size = sizeof(T) * write_size;
-    auto next_ptr = w_pointer + total_size + sizeof(TS);
-    if (next_ptr > buffer_size) {
-      // buffer over
-      throw Exception(true, "[buffer] buffer over!", w_pointer,
-                      total_size + sizeof(TS), buffer_size);
-    }
-    auto base_ptr = reinterpret_cast<uint8_t *>(buffer) + w_pointer;
-    auto size_ptr = reinterpret_cast<TS *>(base_ptr);
-    auto buff_ptr = reinterpret_cast<T *>(size_ptr + 1);
+    auto overall_size = total_size + sizeof(TS);
+    auto next_ptr =
+        get_next_pointer(overall_size, true, "[buffer] buffer over!");
+    auto size_ptr = get_write_pointer<TS>();
     std::memcpy(size_ptr, &write_size, sizeof(TS));
-    std::memcpy(buff_ptr, source_buffer, total_size);
+    if (write_size > 0) {
+      auto buff_ptr = reinterpret_cast<T *>(size_ptr + 1);
+      std::memcpy(buff_ptr, source_buffer, total_size);
+    }
     w_pointer = next_ptr;
   }
   //
   template <class T = char, typename TS = size_t>
   std::pair<const T *, TS> getBuffer() {
-    auto next_ptr = r_pointer + sizeof(TS);
-    auto max_pointer = get_read_max();
-    if (next_ptr > max_pointer) {
-      // buffer over
-      throw Exception(false, "[buffer] buffer over!", r_pointer, sizeof(TS),
-                      max_pointer);
-    }
-    auto base_ptr = reinterpret_cast<uint8_t *>(buffer) + r_pointer;
-    auto size_ptr = reinterpret_cast<TS *>(base_ptr);
+    auto next_ptr =
+        get_next_pointer(sizeof(TS), false, "[buffer] buffer over!");
+    auto size_ptr = get_read_pointer<TS>();
     TS read_size;
     std::memcpy(&read_size, size_ptr, sizeof(TS));
+    r_pointer = next_ptr;
     if (read_size > 0) {
       auto total_size = read_size * sizeof(T);
-      next_ptr += total_size;
-      if (next_ptr > max_pointer) {
-        // buffer over
-        throw Exception(false, "[buffer] buffer over!", r_pointer + sizeof(TS),
-                        total_size, max_pointer);
-      }
+      next_ptr = get_next_pointer(total_size, false, "[buffer] buffer over!");
       auto buff_ptr = reinterpret_cast<T *>(size_ptr + 1);
       r_pointer = next_ptr;
       return std::make_pair(buff_ptr, read_size);
     }
     return std::make_pair(nullptr, 0);
   }
+
   // struct
   template <class T> void putStruct(const T &s) {
     uint16_t ssize = sizeof(T);
-    auto next_ptr = w_pointer + ssize + sizeof(uint16_t);
-    if (next_ptr > buffer_size) {
-      // buffer over
-      throw Exception(true, "[struct] buffer over!", w_pointer,
-                      ssize + sizeof(uint16_t), buffer_size);
-    }
-    auto base_ptr = reinterpret_cast<uint8_t *>(buffer) + w_pointer;
-    auto size_ptr = reinterpret_cast<uint16_t *>(base_ptr);
+    auto overall_size = ssize + sizeof(uint16_t);
+    auto next_ptr =
+        get_next_pointer(overall_size, true, "[struct] buffer over!");
+    auto size_ptr = get_write_pointer<uint16_t>();
     auto buff_ptr = reinterpret_cast<T *>(size_ptr + 1);
     std::memcpy(size_ptr, &ssize, sizeof(uint16_t));
     std::memcpy(buff_ptr, &s, ssize);
@@ -146,30 +138,21 @@ public:
   }
   //
   template <class T> void getStruct(T &result) {
-    auto next_ptr = r_pointer + sizeof(uint16_t);
-    auto max_pointer = get_read_max();
-    if (next_ptr > max_pointer) {
-      // buffer over
-      throw Exception(false, "[struct] buffer over!", r_pointer,
-                      sizeof(uint16_t), max_pointer);
-    }
-    auto base_ptr = reinterpret_cast<uint8_t *>(buffer) + r_pointer;
-    auto size_ptr = reinterpret_cast<uint16_t *>(base_ptr);
+    auto next_ptr =
+        get_next_pointer(sizeof(uint16_t), false, "[struct] buffer over!");
+    auto size_ptr = get_read_pointer<uint16_t>();
     uint16_t struct_size;
     std::memcpy(&struct_size, size_ptr, sizeof(uint16_t));
+    r_pointer = next_ptr;
     if (struct_size > 0) {
-      next_ptr += struct_size;
-      if (next_ptr > max_pointer) {
-        // buffer over
-        throw Exception(false, "[struct] buffer over!",
-                        r_pointer + sizeof(uint16_t), struct_size, max_pointer);
-      }
+      next_ptr = get_next_pointer(struct_size, false, "[struct] buffer over!");
       if (struct_size > sizeof(T))
         struct_size = sizeof(T);
-      memcpy(&result, reinterpret_cast<T *>(size_ptr + 1), struct_size);
+      std::memcpy(&result, reinterpret_cast<T *>(size_ptr + 1), struct_size);
       r_pointer = next_ptr;
     }
   }
+
   // vector
   template <class T, typename TS = size_t>
   void putVector(const std::vector<T> &v) {
@@ -179,6 +162,6 @@ public:
   template <class T, typename TS = size_t> void getVector(std::vector<T> &v) {
     auto r = getBuffer<T, TS>();
     v.resize(r.second);
-    memcpy(v.data(), r.first, sizeof(T) * r.second);
+    std::memcpy(v.data(), r.first, sizeof(T) * r.second);
   }
 };
