33297f2a3553d277469af8f7e3ec5fe5f12c4698
Yoshinori Suzuki <wave.suzuki.z@gmail.com>
2018-11-03 08:39:48 +0000

    modify lua script

--- lua/struct.lua
+++ lua/struct.lua
@@ -1,9 +1,8 @@
 -- test
 print(string.format("Hello LUA(%s)",ExecuteFilename))
 print("Count:",gTest.count)
-print("Line2:",gTest.line[2],#gTest.line)
+print("Player:",gTest.child.name,"Age:",gTest.child.age)
 print("BeerType:",gTest.beer_type)
-print("Child:",gTest.child.name,"Age:",gTest.child.age)
 if gTest.beer_type == Test.BeerType.Larger
 then
     print "is Larger"
@@ -12,10 +11,19 @@ else
     print "no Larger"
     gTest.beer_type = Test.BeerType.Larger
 end
-print("Note2:",gTest.note[2].page,"line=",gTest.note[2].line)
-print "Top10:"
-for r = 1,10 do
-    print(r,"=",gTest.ranking[r])
+print(string.format("BeerType[Lambic] is %d",Test.BeerType.Lambic))
+print("Line:",#gTest.line)
+for li, l in ipairs(gTest.line) do
+    print(string.format("  %0.3f",l))
+end
+print("Note:")
+for ni, n in ipairs(gTest.note) do
+    print(string.format("  note[%d]: %3d,%2d",ni,n.page,n.line))
+end
+local pl_num = #gTest.entry_list
+print(string.format("Top%d:",pl_num))
+for r = 1,pl_num do
+    print(string.format("  %2d = %d",r,gTest.ranking[r]))
 end
 -- local e = TestEntry.new()
 -- e.name = "K. Yamada"
@@ -26,7 +34,7 @@ for ei, ev in ipairs(gTest.entry_list) do
     local m = string.format("    [%i]: %s(%s): %d pts,%d wins",ei,ev.name,ev.country,ev.point,ev.wins)
     print(m)
 end
-print(Test.BeerType.Lambic)
+print("Args:")
 for ai, arg in ipairs(args) do
     print(string.format("    arg[%d]: %s",ai,arg))
 end
