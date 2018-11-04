e78936234709d5e79930874bc6058756dc9db1c6
Yoshinori Suzuki <wave.suzuki.z@gmail.com>
2018-11-04 13:32:34 +0000

    script update

--- lua/struct.lua
+++ lua/struct.lua
@@ -1,18 +1,25 @@
 -- test
 print(string.format("Hello LUA(%s)",ExecuteFilename))
-print("Count:",gTest.count)
+print(string.format("Count: %d",gTest.count))
+if #args > 0 then
+    print("Args:")
+    for ai, arg in ipairs(args) do
+        print(string.format("    arg[%d]: %s",ai,arg))
+    end
+else
+    print("no args")
+end
 print("Player:",gTest.child.name,"Age:",gTest.child.age)
-print("BeerType:",gTest.beer_type)
 if gTest.beer_type == Test.BeerType.Larger
 then
-    print "is Larger"
+    print(string.format("Beer is Larger"))
     gTest.beer_type = Test.BeerType.Pilsner
 else
-    print "no Larger"
+    print(string.format("Beer is no Larger(type=%d)",gTest.beer_type))
     gTest.beer_type = Test.BeerType.Larger
 end
 print(string.format("BeerType[Lambic] is %d",Test.BeerType.Lambic))
-print("Line:",#gTest.line)
+print(string.format("Line: %d",#gTest.line))
 for li, l in ipairs(gTest.line) do
     print(string.format("  %0.3f",l))
 end
@@ -29,13 +36,9 @@ end
 -- e.name = "K. Yamada"
 -- e.country = "JP"
 -- table.insert(gTest.entry_list,e)
-print("EntryList:",#gTest.entry_list)
+print(string.format("EntryList: %d players",#gTest.entry_list))
 for ei, ev in ipairs(gTest.entry_list) do
-    local m = string.format("    [%i]: %s(%s): %d pts,%d wins",ei,ev.name,ev.country,ev.point,ev.wins)
+    local m = string.format("    [%i]: %-16s(%s): %3d pts,%d wins",ei,ev.name,ev.country,ev.point,ev.wins)
     print(m)
-end
-print("Args:")
-for ai, arg in ipairs(args) do
-    print(string.format("    arg[%d]: %s",ai,arg))
 end
 print "Good bye LUA"
